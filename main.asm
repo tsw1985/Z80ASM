67b7d2aea8c314dcd49d89c971f3fb1275e7b04a
Gabriel Glez <gabrielgg85@gmail.com>
2026-04-14 18:14:07 +0100

    First Commit

---
+++ main.asm
@@ -0,0 +1,29 @@
+    DEVICE ZXSPECTRUM48
+
+    ORG 0x8000
+
+main:
+    LD SP, 0xFFFF
+
+    LD A, 'H'
+    RST 0x10
+
+    LD A, 'O'
+    RST 0x10
+
+    LD A, 'L'
+    RST 0x10
+
+    LD A, 'A'
+    RST 0x10
+
+    LD A, 'A'
+    RST 0x10
+
+    LD A, 'A'
+    RST 0x10
+
+loop:
+    JR loop
+
+    SAVESNA "hola.sna", main
